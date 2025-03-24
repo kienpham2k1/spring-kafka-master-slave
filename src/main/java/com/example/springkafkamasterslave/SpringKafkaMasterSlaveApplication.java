bd2189769f59f21aa79652b81d8e030f5901984a
KienPT <kienpt.dev.01@gmail.com>
2025-03-24 21:06:43 +0700

    init prj

---
+++ src/main/java/com/example/springkafkamasterslave/SpringKafkaMasterSlaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkamasterslave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaMasterSlaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaMasterSlaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springkafkamasterslave/SpringKafkaMasterSlaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkamasterslave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaMasterSlaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
